cf9e621729c91f9461d3a70b231d66777397f4ea
charles <charles.mirandafroes@gmail.com>
2021-07-12 18:59:19 -0300

    Added comment

--- src/main/java/com/inss/service/CalculateService.java
+++ src/main/java/com/inss/service/CalculateService.java
@@ -27,39 +27,39 @@ public class CalculateService {
                         var employeeResponse = EmployeeResponse.builder().build();
                         employeeResponse.setId(employee.getId());
                         employeeResponse.setSalary(employee.getSalary());
+
+                        // Until R$ 1.100 discounts 7.50%
                         if (employee.getSalary().compareTo(inss.getUntil()) == -1 ||
                                 employee.getSalary().compareTo(inss.getUntil()) == 0
                         ) {
-
                             employeeResponse.setDiscount(employee.getSalary().multiply(inss.getPercent()
                                     .divide(new BigDecimal(100))).setScale(2, RoundingMode.HALF_EVEN));
                             employeeResponse.setPercent(inss.getPercent());
 
-                        } else if ((employee.getSalary().compareTo(inss.getFromSecond()) == 0 ||
+                        } // From R$ 1.100.01 until R$ 2.203,48 discounts 9%
+                        else if ((employee.getSalary().compareTo(inss.getFromSecond()) == 0 ||
                                 employee.getSalary().compareTo(inss.getFromSecond()) == 1) &&
                                 (employee.getSalary().compareTo(inss.getUntilSecond()) == 0 ||
                                         employee.getSalary().compareTo(inss.getUntilSecond()) == -1)) {
-
                             employeeResponse.setDiscount(employee.getSalary().multiply(inss.getPercentSecond()
                                     .divide(new BigDecimal(100))).setScale(2, RoundingMode.HALF_EVEN));
                             employeeResponse.setPercent(inss.getPercentSecond());
 
-                        } else if ((employee.getSalary().compareTo(inss.getFromThird()) == 0 ||
+                        } // From R$ 2.203,49 until R$ 3.305,22 discounts 12%
+                        else if ((employee.getSalary().compareTo(inss.getFromThird()) == 0 ||
                                 employee.getSalary().compareTo(inss.getFromThird()) == 1) &&
                                 (employee.getSalary().compareTo(inss.getUntilThird()) == 0 ||
                                         employee.getSalary().compareTo(inss.getUntilThird()) == -1)) {
-
                             employeeResponse.setDiscount(employee.getSalary().multiply(inss.getPercentThird()
                                     .divide(new BigDecimal(100))).setScale(2, RoundingMode.HALF_EVEN));
                             employeeResponse.setPercent(inss.getPercentThird());
 
-                        } else if ((employee.getSalary().compareTo(inss.getFromFourth()) == 0 ||
+                        } // From R$ 3.305,23 until R$ 6.433,57 and above discounts 14%
+                        else if ((employee.getSalary().compareTo(inss.getFromFourth()) == 0 ||
                                 employee.getSalary().compareTo(inss.getFromFourth()) == 1)) {
-
                             employeeResponse.setDiscount(employee.getSalary().multiply(inss.getPercentFourth()
                                     .divide(new BigDecimal(100))).setScale(2, RoundingMode.HALF_EVEN));
                             employeeResponse.setPercent(inss.getPercentFourth());
-
                         }
 
                         calculateResponse.getEmployees().add(employeeResponse);
