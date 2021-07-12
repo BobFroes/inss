787dd14322b87a6db6e8acbbadbf4f3555112aa1
charles <charles.mirandafroes@gmail.com>
2021-07-12 10:34:20 -0300

    Modified integration test

--- src/test/java/intg/com/inss/kafka/ConsumerIntegrationTest.java
+++ src/test/java/intg/com/inss/kafka/ConsumerIntegrationTest.java
@@ -6,28 +6,27 @@ import com.inss.builder.InssBuilder;
 import com.inss.common.constant.Topics;
 import com.inss.db.CustomPostgresContainer;
 import com.inss.domain.InssRepository;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.junit.jupiter.api.Assertions;
+import com.inss.service.CalculateService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.util.Map;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @Testcontainers
 @DirtiesContext
 @SpringBootTest
-@EmbeddedKafka(topics = { Topics.INSS_CALCULATE, Topics.INSS_RESPONSE})
+@EmbeddedKafka(topics = {Topics.INSS_CALCULATE, Topics.INSS_RESPONSE},
+        partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 class ConsumerIntegrationTest {
 
     @Container
@@ -35,8 +34,14 @@ class ConsumerIntegrationTest {
 
     private CalculateBuilder builder;
 
-    private KafkaProducer<String, String> producer;
-    private KafkaConsumer<String, String> consumer;
+    @SpyBean
+    private Consumer consumer;
+
+    @SpyBean
+    private CalculateService service;
+
+    @SpyBean
+    private Producer producer;
 
     @Autowired
     private EmbeddedKafkaBroker embeddedKafkaBroker;
@@ -48,25 +53,20 @@ class ConsumerIntegrationTest {
     void setUp() {
         repository.save(new InssBuilder().get());
         builder = new CalculateBuilder();
-
-        Map<String, Object> producerProps = KafkaTestUtils.producerProps(embeddedKafkaBroker);
-        producer = new KafkaProducer<>(producerProps);
-
-        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("inss-group", "false", embeddedKafkaBroker);
-        consumer = new KafkaConsumer<>(consumerProps);
-        embeddedKafkaBroker.consumeFromAnEmbeddedTopic(consumer, Topics.INSS_CALCULATE);
     }
 
     @Test
     void it_should_calculate_successfully() throws Exception {
-        producer.send(new ProducerRecord<>(Topics.INSS_CALCULATE, 0, null, new Gson().toJson(builder.withEmployees())));
+        var employees = builder.withEmployees();
 
-        var records = KafkaTestUtils.getRecords(consumer, 10_000L, 1);
+        consumer.receive(new Gson().toJson(employees));
 
-        Assertions.assertEquals(1, records.count());
+        verify(service, times(1)).execute(employees);
 
-        producer.close();
-        consumer.close();
+        var response = service.execute(employees);
+
+        verify(producer, times(1)).send(Topics.INSS_RESPONSE, response);
+
     }
 
 }
