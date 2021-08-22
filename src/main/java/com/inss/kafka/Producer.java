2500a52eb8b74a151322ab67caae59a64cae03ec
charles <charles.mirandafroes@gmail.com>
2021-08-22 17:57:56 -0300

    listenableFuture returned removed

--- src/main/java/com/inss/kafka/Producer.java
+++ src/main/java/com/inss/kafka/Producer.java
@@ -4,11 +4,9 @@ import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
 
 @Component
 public class Producer {
@@ -16,14 +14,14 @@ public class Producer {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    public ListenableFuture<SendResult<String, String>> send(String topic, Object request) {
+    public void send(String topic, Object request) {
 
         Message<String> message = MessageBuilder
                 .withPayload(new Gson().toJson(request))
                 .setHeader(KafkaHeaders.TOPIC, topic)
                 .build();
 
-        return this.kafkaTemplate.send(message);
+        this.kafkaTemplate.send(message);
 
     }
 
